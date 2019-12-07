b7d63beb8fd87a595ec78b73222b4fbafb0eb166
Surfsol <surfsol@protonamil.com>
2019-12-07 16:28:47 -0300

    Add Landing.js, Add assets/css/landingCss.scss

---
+++ src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+const Landing = () => {
+    return(
+        <>
+        <div className="image">
+            <img src= "https://res.cloudinary.com/dfulxq7so/image/upload/v1572452572/malawi20100165_cesh8j.jpg"></img>
+        </div>
+        </>
+
+    )
+}
+export default Landing

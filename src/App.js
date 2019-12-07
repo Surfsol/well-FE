62e3907dccf1da747959d2d0401cc740ce42ab35
Surfsol <surfsol@protonamil.com>
2019-12-07 17:37:34 -0300

    yarn add @material-ui, @material-ui/icons, Add SignInSide, remove Landing.js from app.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 import Landing from './components/Landing'
+import SignInSide from './components/SignInSide'
 
 function App() {
   return (
     <div>
-      <Landing/>
+      <SignInSide/>
 
     </div>
   );

5ffdd31270cde0f1e29ad3bcda4a45634e0e7b6a
Surfsol <surfsol@protonamil.com>
2019-12-09 06:13:32 -0300

    Added to Dashboard.js seachFiltered and history and zoom

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,36 +1,70 @@
-import React, {useState, useEffect} from 'react'
-import AxiosWithAuth from './AxiosWithAuth'
-import Map from './Map'
+import React, { useState, useEffect } from "react";
+import AxiosWithAuth from "./AxiosWithAuth";
+import Map from "./Map";
 
 const Dashboard = props => {
-    const [viewport, setViewport] = useState({
+  const [viewport, setViewport] = useState({
+    width: "100vw",
+    height: "100vh",
+    latitude: 13.004758,
+    longitude: 105.784788,
+    zoom: 8
+  });
+
+  const [serchFiltered, setSearchFiltered] = useState([]);
+  const [senorInDashboard, setSensorInDashboard] = useState([]);
+  const [history, setHistory] = useState([]);
+
+  //get senors
+  useEffect(() => {
+    AxiosWithAuth()
+      .get("https://welldone-db.herokuapp.com/api/sensors/recent")
+      .then(res => {
+        console.log(`dashboard`, res.data);
+        setSensorInDashboard(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  useEffect(() => {
+    AxiosWithAuth()
+      .get("https://welldone-db.herokuapp.com/api/history")
+      .then(res => {
+        setHistory(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  const zoomInto = () => {
+    if (searchFiltered.length == 0) {
+      setViewport({
+        latitude: 13.5651,
+        longitude: 104.7538,
         width: "100vw",
-      height: "100vh",
-      latitude: 13.004758,
-      longitude: 105.784788,
-      zoom: 8
-    })
+        height: "100vh",
+        zoom: 8
+      });
+    } else if (searchFiltered.length == 1) {
+      const searchPlace = {
+        latitude: searchFiltered[0].latitude,
+        longitude: searchFiltered[0].longitude,
+        width: "100vw",
+        height: "100vh",
+        zoom: 11
+      };
+      setViewport(searchFiltered);
+    }
+    else if(searchFiltered.length > 1) {
+        function avgCoordinate(arr){
+            var totalLat = 0
+        }
+    }
+  };
 
-    const [senorInDashboard, setSensorInDashboard]=useState([])
-
-
-    //get senors 
-    useEffect(()=> {
-        AxiosWithAuth()
-        .get("https://welldone-db.herokuapp.com/api/sensors/recent")
-        .then(res => {
-            console.log(`dashboard`, res.data)
-            setSensorInDashboard(res.data)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-    }, []);
-    return(
-        <>
-        </>
-    )
-
-
-}
-export default Dashboard
+  return <></>;
+};
+export default Dashboard;

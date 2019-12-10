1e7c60d134a17bd98f3ce443396894ebc149f6ac
Surfsol <surfsol@protonamil.com>
2019-12-10 18:47:53 -0300

    Move MonitorDetails component, App.js add monitordetails

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Route, Switch} from "react-router-dom"
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
 
-import SignInSide from './components/SignInSide'
-import Map from './components/Map'
-import Dashboard from './components/Dashboard'
+import SignInSide from "./components/SignInSide";
+import Map from "./components/Map";
+import Dashboard from "./components/Dashboard";
 
-
+import MonitorDetails from "./components/MonitorDetails";
 
 function App() {
   //selectedPump -> MonitorDetails, Dashboard -> Map
-  const[selectedPump, setSelectedPump] = useState([])
+  const [selectedPump, setSelectedPump] = useState([]);
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={SignInSide}/>
-        <Route path="/dashboard" 
-        selectedPump= {selectedPump}
-        setSelectedPump={setSelectedPump}
-        component={Dashboard}/>
-        <Route path="/map" component={Map}/>
+        <Route exact path="/" component={SignInSide} />
+        <Route
+          path="/dashboard"
+          selectedPump={selectedPump}
+          setSelectedPump={setSelectedPump}
+          component={Dashboard}
+        />
+        <Route path="/map" component={Map} />
+
+        <PrivateRoute
+          path="monitorDetails"
+          page={MonitorDetails}
+          selectedPump={selectedPump}
+        />
       </Switch>
     </div>
   );
